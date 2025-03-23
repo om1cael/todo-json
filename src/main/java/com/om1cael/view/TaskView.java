0010d5cdf1617a47c31154fdf3d1cb47a980c85b
om1cael <188113439+om1cael@users.noreply.github.com>
2025-03-23 19:26:37 -0300

    feat(TaskView): inject TaskController

--- src/main/java/com/om1cael/view/TaskView.java
+++ src/main/java/com/om1cael/view/TaskView.java
@@ -1,14 +1,17 @@
 package com.om1cael.view;
 
+import com.om1cael.controller.TaskController;
 import com.om1cael.model.Task;
 import com.om1cael.model.TaskPriority;
 import com.om1cael.utils.InputParser;
 
 public class TaskView {
     private final InputParser inputParser;
+    private final TaskController taskController;
 
-    public TaskView(InputParser inputParser) {
+    public TaskView(InputParser inputParser, TaskController taskController) {
         this.inputParser = inputParser;
+        this.taskController = taskController;
     }
 
     public void addTask() {
@@ -18,5 +21,6 @@ public class TaskView {
         int priority = inputParser.getNumber("Priority (" + 0 + "-" + priorities +"): ", 0, priorities);
 
         Task task = new Task(0, description, TaskPriority.values()[priority]);
+        this.taskController.addTask(task);
     }
 }
