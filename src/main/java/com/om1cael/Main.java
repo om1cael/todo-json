ba1ea5c8a1edd5945da5629d27aa578777b8856f
om1cael <188113439+om1cael@users.noreply.github.com>
2025-03-23 19:26:54 -0300

    feat(Main): inject StorageService, TaskController, TaskView

--- src/main/java/com/om1cael/Main.java
+++ src/main/java/com/om1cael/Main.java
@@ -1,7 +1,11 @@
 package com.om1cael;
 
+import com.google.gson.Gson;
+import com.om1cael.controller.TaskController;
+import com.om1cael.service.StorageService;
 import com.om1cael.utils.InputParser;
 import com.om1cael.view.MenuView;
+import com.om1cael.view.TaskView;
 
 import java.util.Scanner;
 
@@ -9,7 +13,12 @@ public class Main {
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)) {
             InputParser inputParser = new InputParser(scanner);
-            new MenuView(inputParser).showMenu();
+            StorageService storageService = new StorageService(new Gson());
+
+            TaskController taskController = new TaskController(storageService);
+            TaskView taskView = new TaskView(inputParser, taskController);
+
+            new MenuView(inputParser, taskView).showMenu();
         }
     }
 }
